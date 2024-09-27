bf06b4f69fe3300bfce8c614e953a80c1ee1d9a3
erobinofficial <erobin8778@gmail.com>
2024-09-27 22:04:27 +0600

    first commit

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import auth from "../../firebase.config";
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -16,6 +16,8 @@ const Register = () => {
     // const username = e.target.username.value;
     const password = e.target.password.value;
     const accept = e.target.terms.checked;
+    const name = e.target.name.value;
+    console.log(name);
     // console.log(email, username, password);
     console.log(password.length);
     // reset error and success messages
@@ -42,6 +44,13 @@ const Register = () => {
       .then((result) => {
         setRegisterSuccess('You are registered successfully');
         console.log(result);
+        updateProfile(result.user,{
+          displayName : name, photoURL : "omuk.jpg"
+        })
+        .then(()=>{
+          console.log('profile updated');
+        })
+        .catch();
         sendEmailVerification(result.user)
         .then(()=>{
           alert('Sent verification link');
@@ -74,6 +83,23 @@ const Register = () => {
             className="grow"
             placeholder="Email"
             name="email" required
+          />
+        </label>
+        <label className="input input-bordered flex items-center gap-2 mt-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70"
+          >
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+          </svg>
+          <input
+            type="text"
+            className="grow"
+            placeholder="Full name"
+            name="name" required
           />
         </label>
         {/* <label className="input input-bordered flex items-center gap-2 mt-2">
